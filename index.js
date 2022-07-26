c33ad9c10a612197e02500cc28ee05a94e039f37
Findyourcrush69 <110018484+Findyourcrush69@users.noreply.github.com>
2022-07-26 13:03:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector(".btn1").addEventListener("click", handleClick);
+
+function handleClick() {
+  prompt("Enter your name");
+}
+
+document.querySelector(".btn2").addEventListener("click", handleClick2);
+
+function handleClick2() {
+  prompt("Enter your Crush's name");
+}
